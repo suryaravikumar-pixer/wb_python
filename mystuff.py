7e8f2124c027de1fe3ab0b5bcf0af880f82931cb
suryaravikumar-pixer <suryaravikumar@gmail.com>
2021-05-30 16:46:51 +0530

    first version of mystuff file

---
+++ mystuff.py
@@ -0,0 +1,8 @@
+class MyStuff:
+
+    def __init__(self):
+        self.continent_food = "Bolo ganesh!!"
+
+    def apple(self):
+        print("I love apples!")
+
---
+++ mystuff_apple.py
@@ -0,0 +1,35 @@
+from mystuff import MyStuff
+
+thing=MyStuff()
+thing.apple()
+print(thing.continent_food)
+"""type of our class"""
+# print(type(mystuff))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if __name__ == "__main__":
+#    print("File one executed when ran directly")
+#    thing=mystuff.MyStuff()
+#    thing.apple()
+#    print(thing.continent_food)
+# else:
+#    print("File one executed when imported")
+
+
+
