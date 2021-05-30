35826afb6450ec251162a1e8364bf234b848f452
suryaravikumar-pixer <suryaravikumar@gmail.com>
2021-05-30 21:11:35 +0530

    __name__ code is deleted

--- mystuff_apple.py
+++ mystuff_apple.py
@@ -9,31 +9,3 @@ seller=vender('mitchel','r12xa20490')
 seller.selling()
 """type of our class"""
 # print(type(mystuff))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# if __name__ == "__main__":
-#    print("File one executed when ran directly")
-#    thing=mystuff.MyStuff()
-#    thing.apple()
-#    print(thing.continent_food)
-# else:
-#    print("File one executed when imported")
-
-
-
