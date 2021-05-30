61c5964239a8a9e55c6e97a5c116b3c6b9c7b3fa
suryaravikumar-pixer <suryaravikumar@gmail.com>
2021-05-30 19:53:23 +0530

    seller name, is are listed

--- mystuff_apple.py
+++ mystuff_apple.py
@@ -1,10 +1,12 @@
-from mystuff import MyStuff
+from mystuff import MyStuff, vender
 
 thing=MyStuff()
 thing.apple()
 thing.orange()
-thing.vender()
 print(thing.continent_food)
+print("------------------------")
+seller=vender('mitchel','r12xa20490')
+seller.selling()
 """type of our class"""
 # print(type(mystuff))
 
